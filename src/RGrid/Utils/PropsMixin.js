52484aacee2e9515151dcc303e9ec9fb7bf29d84
Sarp Yiğit İlhan <sarpilhan@ready.com.tr>
2019-11-06 10:55:25 +0300

    Grid Data Method

--- src/RGrid/Utils/PropsMixin.js
+++ src/RGrid/Utils/PropsMixin.js
@@ -1,27 +1,30 @@
 export default {
   props: {
     columns: { type: Array, required: true },
-    dataset: { type: Array, required: true }, 
+    dataset: { type: Array, required: true },
     total: { type: Number, required: true },
     pageSize: { type: Array, required: true },
-    query: { type: Object, required: true, default: { limit: 0, offset: 0, sort: [], filter: [] },
-    summary: Object, 
-    xprops: Object, 
-    tableClass: [String, Object, Array], 
-    tableStyle: [String, Object, Array],  
+    query: {
+      type: Object, required: true, default: { limit: 0, offset: 0, sort: [], filter: [] }
+    },
+    summary: Object,
+    xprops: Object,
+    tableClass: [String, Object, Array],
+    tableStyle: [String, Object, Array],
     isResponsive: [String, Boolean],
-    isServerSide: [String, Boolean]
+    isServerSide: [String, Boolean],
+    dataMethod: { type: Function, required: false, default: null }
   },
   data() {
     let datatableInstance = this
     while (datatableInstance.$options.name !== 'RGrid') {
-      datatableInstance = datatableInstance.$parent 
+      datatableInstance = datatableInstance.$parent
     }
     return {
       // the source of dynamic components (thComp / tdComp / nested components)
       comp: datatableInstance.$parent.$options.components
     }
-  }, 
+  },
   methods: {
     // usage: <component :is="forDynCompIs(XXX)" ... />
     forDynCompIs(component) {
